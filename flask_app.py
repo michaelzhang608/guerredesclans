7de200c9d47e4c1411e49cea9c82e43969b02fd3
michaelzhang608 <michaelzhang608@gmail.com>
2019-01-29 16:12:32 +0000

    first commit

---
+++ flask_app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request
+import os
+from utils import check_pass, read_csv, write_csv
+import time
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret123'
+
+@app.route("/")
+def index():
+    return render_template("index.html", points=get_points())
+
+@app.route("/update_points", methods=["POST"])
+def add_points():
+    points = request.form.get("points")
+    clan = request.form.get("clan")
+    password = request.form.get("password")
+    result = add_points([clan, points], password)
+
+    return render_template("index.html", points=get_points())
+
+def get_points(clan=None):
+    # Get points
+    f = read_csv("/home/guerredesclans/mysite/db.csv")
+    m = max([int(i[1]) for i in f])
+    # Compute percents
+    out = {k: (int(v), round(100 * int(v) / m)) for k, v in f}
+    if not clan:
+        return [[k, v] for k,v in out.items()]
+    else:
+        return out[clan]
+
+def add_points(change, password):
+    if check_pass("/home/guerredesclans/mysite/password.txt", password):
+        points = [[p[0], p[1][0]] for p in get_points()]
+        for p in points:
+            if p[0] == change[0]:
+                p[1] = p[1] + int(change[1])
+        log = read_csv("/home/guerredesclans/mysite/log.csv")
+        log.append([datetime.now(), change[0], change[1]])
+        write_csv("/home/guerredesclans/mysite/log.csv", log)
+        write_csv("/home/guerredesclans/mysite/db.csv", points)
+        return change[0]
+    else:
+        return False
+
+if __name__ == "__main__":
+    os.environ["FLASK_APP"] = "flask_app.py"
+    os.system("flask run")
---
+++ utils.py
@@ -0,0 +1,49 @@
+import csv
+import hashlib
+
+def check_pass(solution_dir, attempt):
+
+    # Get hashed password
+    with open(solution_dir, "r") as f:
+        for l in f:
+            hex_pass_true = l
+
+    # Hash input password and check if match
+    hex_pass = hashlib.sha256(attempt.encode()).hexdigest()
+    if hex_pass == hex_pass_true:
+        return True
+
+    return False
+
+def read_csv(filename):
+        out = []
+        with open(filename, "r") as f:
+            r = csv.reader(f)
+            for l in r:
+                out.append(l)
+        return out
+
+def write_csv(filename, content):
+    with open(filename, "w") as f:
+        w = csv.writer(f)
+        for l in content:
+            w.writerow(l)
+    return read_csv(filename)
+
+def clear_csv(filename):
+    with open(filename, "r+") as f:
+        f.truncate(0)
+
+def get_input(prompt, is_int=False, int_range=None):
+    while True:
+        out = input(prompt)
+        if out:
+            if is_int:
+                try:
+                    out = int(out)
+                except ValueError:
+                    continue
+                if int_range:
+                    if int_range[0] > out or out > int_range[1]:
+                        continue
+            return out
