fb0899d9a3fb6c57d8c879fd0da4b0e21db577eb
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-01-29 14:33:43 -0500

    computer first commit

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,7 @@ from utils import check_pass, read_csv, write_csv
 import time
 from datetime import datetime
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret123'
 
@@ -22,7 +23,7 @@ def add_points():
 
 def get_points(clan=None):
     # Get points
-    f = read_csv("/home/guerredesclans/mysite/db.csv")
+    f = read_csv("/static/db.csv")
     m = max([int(i[1]) for i in f])
     # Compute percents
     out = {k: (int(v), round(100 * int(v) / m)) for k, v in f}
@@ -32,19 +33,20 @@ def get_points(clan=None):
         return out[clan]
 
 def add_points(change, password):
-    if check_pass("/home/guerredesclans/mysite/password.txt", password):
+    if check_pass("/static/password.txt", password):
         points = [[p[0], p[1][0]] for p in get_points()]
         for p in points:
             if p[0] == change[0]:
                 p[1] = p[1] + int(change[1])
         log = read_csv("/home/guerredesclans/mysite/log.csv")
         log.append([datetime.now(), change[0], change[1]])
-        write_csv("/home/guerredesclans/mysite/log.csv", log)
-        write_csv("/home/guerredesclans/mysite/db.csv", points)
+        write_csv("/static/log.csv", log)
+        write_csv("/static/db.csv", points)
         return change[0]
     else:
         return False
 
 if __name__ == "__main__":
     os.environ["FLASK_APP"] = "flask_app.py"
+    
     os.system("flask run")
