7725f3659d66496cb0dde8004556fd443fdb89fb
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-01-29 15:56:31 -0500

    change file format

--- flask_app.py
+++ flask_app.py
@@ -23,7 +23,11 @@ def add_points():
 
 def get_points(clan=None):
     # Get points
-    f = read_csv("/static/db.csv")
+
+    if is_production():
+        f = read_csv('assets/db.csv')
+    else:
+        f = read_csv('static/db.csv')
     m = max([int(i[1]) for i in f])
     # Compute percents
     out = {k: (int(v), round(100 * int(v) / m)) for k, v in f}
@@ -33,20 +37,38 @@ def get_points(clan=None):
         return out[clan]
 
 def add_points(change, password):
-    if check_pass("/static/password.txt", password):
+
+    pass_location = 'static/password.txt'
+    log_location = 'static/log.csv'
+    db_location = 'static/db.csv'
+
+    if is_production():
+        pass_location = 'assets/password.txt'
+        log_location = 'assets/log.csv'
+        db_location = 'assets/db.csv'
+
+    if check_pass(pass_location, password):
         points = [[p[0], p[1][0]] for p in get_points()]
         for p in points:
             if p[0] == change[0]:
                 p[1] = p[1] + int(change[1])
-        log = read_csv("/home/guerredesclans/mysite/log.csv")
+
+        # Log update
+        log = read_csv(log_location)
         log.append([datetime.now(), change[0], change[1]])
-        write_csv("/static/log.csv", log)
-        write_csv("/static/db.csv", points)
+
+        write_csv(log_location, log)
+        write_csv(db_location, points)
         return change[0]
     else:
         return False
 
+def is_production():
+    root_url = request.url_root
+    developer_url = 'http://127.0.0.1:5000/'
+    return root_url == developer_url
+
+
 if __name__ == "__main__":
     os.environ["FLASK_APP"] = "flask_app.py"
-    # LOCAL FILE
     os.system("flask run")
