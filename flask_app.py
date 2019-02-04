490c006888b5c4afd8ae1db5779529689ae0506d
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-02-03 20:14:49 -0500

    changing file save location

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,6 @@ from utils import check_pass, read_csv, write_csv
 import time
 from datetime import datetime
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret123'
 
@@ -23,13 +22,13 @@ def add_points():
 
 def get_points(clan=None):
     # Get points
-
     if is_production():
-        f = read_csv('assets/db.csv')
-    else:
         f = read_csv('/static/db.csv')
-    m = max([int(i[1]) for i in f])
+    else:
+        f = read_csv('db.csv')
+
     # Compute percents
+    m = max([int(i[1]) for i in f])
     out = {k: (int(v), round(100 * int(v) / m)) for k, v in f}
     if not clan:
         return [[k, v] for k,v in out.items()]
@@ -38,14 +37,14 @@ def get_points(clan=None):
 
 def add_points(change, password):
 
-    pass_location = '/static/password.txt'
-    log_location = '/static/log.csv'
-    db_location = '/static/db.csv'
+    pass_location = 'password.txt'
+    log_location = 'log.csv'
+    db_location = 'db.csv'
 
     if is_production():
-        pass_location = 'assets/password.txt'
-        log_location = 'assets/log.csv'
-        db_location = 'assets/db.csv'
+        pass_location = '/static/password.txt'
+        log_location = '/static/log.csv'
+        db_location = '/how static/db.csv'
 
     if check_pass(pass_location, password):
         points = [[p[0], p[1][0]] for p in get_points()]
@@ -66,7 +65,7 @@ def add_points(change, password):
 def is_production():
     root_url = request.url_root
     developer_url = 'http://127.0.0.1:5000/'
-    return root_url == developer_url
+    return root_url != developer_url
 
 
 if __name__ == "__main__":
