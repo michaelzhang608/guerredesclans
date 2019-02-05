5337a7802bf7b8593b024b3103e8d3cad0a943a0
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-02-04 21:50:34 -0500

    force cache reload

--- flask_app.py
+++ flask_app.py
@@ -65,6 +65,18 @@ def add_points(change, password):
     else:
         return False
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 def is_production():
     root_url = request.url_root
     developer_url = 'http://127.0.0.1:5000/'
