53530e8875ec163cf939738baf673ebaeb06fdb7
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-02-12 17:21:18 -0500

    added extra try catch

--- backup.py
+++ backup.py
@@ -5,11 +5,14 @@ from utils import read_csv
 
 try:
     teams = read_csv("teams.csv")
-    time = datetime.now().strftime("%Y-%m-%d %H:%m:%S")
-    with open("backup_db/" + time + ".csv", "a") as f:
-        w = csv.writer(f)
-        for line in teams:
-            w.writerow(line)
-    print(f"Succesful backup at: {time}")
+    try:
+        time = datetime.now().strftime("%Y-%m-%d %H:%m:%S")
+        with open("backup_db/" + time + ".csv", "a") as f:
+            w = csv.writer(f)
+            for line in teams:
+                w.writerow(line)
+        print(f"Succesful backup at: {time}")
+    except:
+        print(f"Unsuccesful backup at: {time}")
 except:
-    print(f"Unsuccesful backup at: {time}")
+    print("read failed")
