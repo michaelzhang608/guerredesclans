34c35c3009ac660dc83a9a15f6e91495f3846915
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-02-12 17:17:50 -0500

    added backup

---
+++ backup.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+import csv
+
+from utils import read_csv
+
+try:
+    teams = read_csv("teams.csv")
+    time = datetime.now().strftime("%Y-%m-%d %H:%m:%S")
+    with open("backup_db/" + time + ".csv", "a") as f:
+        w = csv.writer(f)
+        for line in teams:
+            w.writerow(line)
+    print(f"Succesful backup at: {time}")
+except:
+    print(f"Unsuccesful backup at: {time}")
