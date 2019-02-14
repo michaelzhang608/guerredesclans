76f41ac152ab6a7cc2f911399c054dc25948feb9
Michael Zhang <michaelzhang@Michaels-MacBook-Air-2.local>
2019-02-13 21:09:36 -0500

    added try another time to backup

--- backup.py
+++ backup.py
@@ -15,4 +15,17 @@ try:
     except:
         print("Unsuccesful backup at:", time)
 except:
-    print("read failed")
+    print("read attempt 1 failed")
+    try:
+        teams = read_csv("/home/guerredesclans/teams.csv")
+        try:
+            time = datetime.now().strftime("%Y-%m-%d %H:%m:%S")
+            with open("/home/guerredesclans/backup_db/" + time + ".csv", "a") as f:
+                w = csv.writer(f)
+                for line in teams:
+                    w.writerow(line)
+            print("Succesful backup at:", time)
+        except:
+            print("Unsuccesful backup at:", time)
+    except:
+        print("read attemp 2 failed")
